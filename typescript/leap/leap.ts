cf9c56578701e0c64e3f2723850ade27487b8da0
ErikaJPB <erikajpinedab@gmail.com>
2023-04-24 10:52:24 -0500

    Leap Year

---
+++ typescript/leap/leap.ts
@@ -0,0 +1,11 @@
+export function isLeap(year: number): boolean {
+  if (year % 4 === 0) {
+    if (year % 100 === 0) {
+      return year % 400 === 0;
+    } else {
+      return true;
+    }
+  } else {
+    return false;
+  }
+}
