fe96226aae67ca1803ce716035d47009b24dbc46
ErikaJPB <erikajpinedab@gmail.com>
2023-04-27 13:54:35 -0500

    space-age

---
+++ typescript/space-age/space-age.ts
@@ -0,0 +1,28 @@
+type Planet =
+  | "mercury"
+  | "venus"
+  | "earth"
+  | "mars"
+  | "jupiter"
+  | "saturn"
+  | "uranus"
+  | "neptune";
+
+const earthYearInSeconds = 31557600;
+
+const orbitalPeriods: Record<Planet, number> = {
+  mercury: 0.2408467,
+  venus: 0.61519726,
+  earth: 1,
+  mars: 1.8808158,
+  jupiter: 11.862615,
+  saturn: 29.447498,
+  uranus: 84.016846,
+  neptune: 164.79132,
+};
+
+export function age(planet: Planet, seconds: number): number {
+  const earthYears = seconds / earthYearInSeconds;
+  const ageInPlanetYears = earthYears / orbitalPeriods[planet];
+  return Number(ageInPlanetYears.toFixed(2));
+}
